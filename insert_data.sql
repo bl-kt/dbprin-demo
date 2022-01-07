3923a9c9d3b489c5388a01888d85d11f6e0dee85
UP2016988 <72519948+UP2016988@users.noreply.github.com>
2022-01-07 13:51:33 +0000

    added stuff

--- insert_data.sql
+++ insert_data.sql
@@ -13,11 +13,11 @@ CREATE TABLE staff
     staff_postcode     VARCHAR(8)  NOT NULL
 );
 
-<<<<<<< HEAD
+
 INSERT INTO staff VALUES ('Jaci', 'Kingsley', 1991-08-19, 012345678, 'jaci@hospital.com', 40, 'Portsmouth Road', 'Hampshire', 'Po1 1ab'),
         ('Anneka', 'Brittain', 1986-12-25, 23456789, 'anneka@hospital.com', 38, 'Stanhope Road', 'Hampshire', 'Po1, 0bc'),
         ();
-=======
+
 INSERT INTO staff VALUES
     ("Jaci", "Kingsley", 1991-08-19, 012345678, "jaci@hospital.com", "40", "Portsmouth Road", "Hampshire", "Po1 1ab"),
     ("Anneka", "Brittain", 1986-12-25, 23456789, "anneka@hospital.com", "38", "Stanhope Road", "Hampshire", "Po1, 0bc"),
@@ -25,7 +25,7 @@ INSERT INTO staff VALUES
     ("Adrienne", "Webster", 1988-03-18, 14340000, "a.auctor@aol.com", "586", "Gravida Street", "Surrey","GU1 1AB"),
     ("Jasmine", "Bryant", 1980-08-07, 7763576353, "arcu.vivamus.sit@protonmail.edu","60", "Aliquam St",  "Sussex","W12 QAB"),
 
-<<<<<<< HEAD
+
 CREATE TABLE ward
 (
     ward_id         SERIAL PRIMARY KEY,
