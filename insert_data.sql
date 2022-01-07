46c0f24b7a17a738fd80eb00d137f62ff2339693
UP2028963 <87422366+UP2028963@users.noreply.github.com>
2022-01-07 20:42:11 +0000

    skeleton structure of inserts

--- insert_data.sql
+++ insert_data.sql
@@ -25,6 +25,32 @@ INSERT INTO staff VALUES
     ("Adrienne", "Webster", 1988-03-18, 14340000, "a.auctor@aol.com", "586", "Gravida Street", "Surrey","GU1 1AB"),
     ("Jasmine", "Bryant", 1980-08-07, 7763576353, "arcu.vivamus.sit@protonmail.edu","60", "Aliquam St",  "Sussex","W12 QAB");
 
+INSERT INTO staff_nurse VALUES
+    (1, TRUE, 0),
+    (4, FALSE, 1);
+
+INSERT INTO staff_doctor VALUES
+    (0),
+    (2),
+    (3);
+
+INSERT INTO doctor_treatment VALUES
+    ((2,0), 'Patient needs to take medication for 7 days'),
+
+INSERT INTO treatment VALUES
+    ('2021-05-24', '2021-05-24', 'paracetemol and cough syrup', 0),
+
+INSERT INTO complaint VALUES
+    ('2021-05-24', 'A dry cough and a flu'),
+
+INSERT INTO general_practitioner VALUES
+    ('Somers Town Health Centre', 'Somerstown Central Community Hub, Tyseley Road', 'Southsea', 'Hampshire', 'PO5 4EZ'),
+
+INSERT INTO patient VALUES
+    ('Tawanda', 'Muyeye', '1948-03-05', 07968152882, 't.muyeye@gmail.com', '50 Hamphire Drive', 'Maidstone', 'Hampshire','ME15 7ES', 0, 1),
+    ('Adrian', 'Burl', '1955-07-02', 07618156822, 'a.burl99@gmail.com', 'North Baddesley', 'Chandlers Ford', 'Hampshire', 'SO52 9JU', 0, 0),
+    ('Tadiwanashe', 'Ditima', '1968-12-12', 07115156832, 't.ditima@gmail.com', '50', 'Shakespeare Road', 'Portsmouth', 'PO1 5LF', 0, 2);
+
 CREATE TABLE ward
 (
     ward_id         SERIAL PRIMARY KEY,
@@ -34,11 +60,15 @@ CREATE TABLE ward
 );
 
 INSERT INTO ward VALUES
-    (1, 'A & E', 'Accident and emergency', 'Operates 24/7'),
-    ('General Surgery', 2, 'For general/basic medical concerns'),
+    ('A & E', 'Accident and emergency', 'Operates 24/7'),
+    ('General Surgery', 1, 'For general/basic medical concerns'),
 
 
-INSERT INTO patient VALUES
-    ('Tawanda', 'Muyeye', '1948-03-05', 07968152882, 't.muyeye@gmail.com', '50 Hamphire Drive', 'Maidstone', 'Hampshire','ME15 7ES'),
-    ('Adrian', 'Burl', '1955-07-02', 07618156822, 'a.burl99@gmail.com', 'North Baddesley', 'Chandlers Ford', 'Hampshire', 'SO52 9JU'),
-    ('Tadiwanashe', 'Ditima', '1968-12-12', 07115156832, 't.ditima@gmail.com', '50', 'Shakespeare Road', 'Portsmouth', 'PO1 5LF'),
+-- not sure what to do here
+INSERT INTO ward_speciality VALUES
+    ()
+
+INSERT INTO patient_complaint VALUES
+    -- not sure what to put here
+    ((0, 0), 'Ummmm....')
+
