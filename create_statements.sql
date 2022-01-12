d07ebd97978ab3709028d553b489f76f3b754d1f
UP2016988 <72519948+UP2016988@users.noreply.github.com>
2022-01-12 12:34:11 +0000

    added comments to create table

--- create_statements.sql
+++ create_statements.sql
@@ -93,6 +93,7 @@ CREATE TABLE doctor_treatment (
 CREATE TABLE patient_complaint (
     patient_id INT NOT NULL REFERENCES patient(patient_id),
     complaint_id INT NOT NULL REFERENCES complaint(complaint_id),
-    PRIMARY KEY(patient_id, complaint_id)
+    PRIMARY KEY(patient_id, complaint_id),
+    pat_com_comments VARCHAR(40)
 );
 
