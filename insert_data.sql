0ead063264f1886a12eea30de5c6b5f9d01bed1b
UP768646 <51681429+UP768646@users.noreply.github.com>
2022-01-12 18:40:27 +0000

    insert statements for patient

--- insert_data.sql
+++ insert_data.sql
@@ -120,6 +120,49 @@ INSERT INTO general_practitioner (gp_name, gp_add1, gp_add2, gp_county, gp_postc
     ('Vernsborne Surgery',
      '65', 'Narnsmouth', 'Hammingdom', 'HA4 L37');
 
+-- PATIENT
+-- Patient table working
+-- These needed to be seperated into 2 insert statements due to some having comments and some not.
+INSERT INTO patient (patient_fname, patient_lname, patient_dob, patient_phone, patient_email, patient_add1, patient_add2, patient_county, patient_postcode, patient_gp, patient_ward) VALUES
+    ('Nowell', 'Bolton', '1989-04-05', '07001112222', 'nowell@bolton.com',
+    '50', 'Tree Close', 'Hampshire', 'PO1 1QW',
+    2, 29),
+    ('Tawanda', 'Muyeye', '1948-03-05', '07968152882', 't.muyeye@gmail.com',
+     '50', 'Hamphire Drive, Maidstone', 'Hampshire','ME15 7ES',
+      1, 30),
+    ('Adrian', 'Burl', '1955-07-02', '07618156822', 'a.burl99@gmail.com',
+     '30', 'North Baddesley', 'Hampshire', 'SO52 9JU',
+      4, 31),
+    ('Tadiwanashe', 'Ditima', '1968-12-12', '07115156832', 't.ditima@gmail.com',
+     '22', 'Shakespeare Road', 'Portsmouth', 'PO1 5LF',
+      3, 32),
+       ('Anthony', 'Pearson', '1970-05-20', '0716286851', 'A.Pearson@yahoo.co.uk',
+     '33', 'Droorey Lane', 'Petersfield', 'UP2 4UJ',
+      1, 34),
+    ('Alexander','Ratliff','1967-10-22', '08008719187','enim@icloud.edu',
+     '78','Faucibus St.','Bedfordshire','GL8 4DD',
+     1, 35),
+       ('Callum','Francis','2001-12-22', '05548858371','vulputate.dui.nec@icloud.co.uk',
+     '45', 'Av Lane', 'Derbyshire','RF6 1AY',
+     3,37);
+
+INSERT INTO patient (patient_fname, patient_lname, patient_dob, patient_phone, patient_email, patient_add1, patient_add2, patient_county, patient_postcode, patient_gp, patient_ward, patient_comments) VALUES
+    ('Paul', 'Jackson', '2000-03-12', '07116786438', 'paulJackson@harvard.edu.us',
+     '93', 'Dreighfield Drive', 'Berkshire', 'SL8 4QT',
+      2, 33, 'Patient must be supervised 24/7'),
+    ('Demetria','Sutton','1986-11-21', '01513565815','dignissim@google.net',
+     '45', 'Williams Road','Rutland','S8 6AE',
+     2,36,
+     'Nullam enim. Sed nulla ante, iaculis nec,'),
+    ('Abel','Garrison','1972-02-22', '01487027927','elementum@icloud.com',
+     '82','Magnis St.','Norfolk','W49 5PU',
+     4,39,
+     'a felis ullamcorper viverra. Maecenas iaculis aliquet'),
+    ('Bertha','Bartlett','1993-10-22', '05687602427','parturient@outlook.edu',
+     '56','Fermentum Ave','Merionethshire','U5V 6YP',
+     3,40,
+     'Duis ac arcu. Nunc mauris. Morbi non sapien');
+
 -- DOCTOR-TREATMENT
 INSERT INTO doctor_treatment (doctor_id, treatment_id) VALUES
 --  combo key (of doctor id, then treatment id), comments
@@ -150,46 +193,7 @@ INSERT INTO patient_complaint (patient_id, complaint_id) VALUES
     ((2, 8), 'preparing treatment'),
     ((3, 9), 'Discharged');
 
--- PATIENT
--- Patient table working
-INSERT INTO patient (patient_fname, patient_lname, patient_dob, patient_phone, patient_email, patient_add1, patient_add2, patient_county, patient_postcode, patient_gp, patient_ward, patient_comments) VALUES
-    ('Nowell', 'Bolton', '1989-04-05', '07001112222', 'nowell@bolton.com',
-    '50', 'Tree Close', 'Hampshire', 'PO1 1QW',
-    2, 11),
-    ('Tawanda', 'Muyeye', '1948-03-05', '07968152882', 't.muyeye@gmail.com',
-     '50', 'Hamphire Drive, Maidstone', 'Hampshire','ME15 7ES',
-      1, 1),
-    ('Adrian', 'Burl', '1955-07-02', '07618156822', 'a.burl99@gmail.com',
-     '30', 'North Baddesley', 'Hampshire', 'SO52 9JU',
-      4, 9),
-    ('Tadiwanashe', 'Ditima', '1968-12-12', '07115156832', 't.ditima@gmail.com',
-     '22', 'Shakespeare Road', 'Portsmouth', 'PO1 5LF',
-      3, 2),
-    ('Paul', 'Jackson', '2000-03-12', '07116786438', 'paulJackson@harvard.edu.us',
-     '93', 'Dreighfield Drive', 'Berkshire', 'SL8 4QT',
-      2, 3, 'Patient must be supervised 24/7'),
-    ('Anthony', 'Pearson', '1970-05-20', '0716286851', 'A.Pearson@yahoo.co.uk',
-     '33', 'Droorey Lane', 'Petersfield', 'UP2 4UJ',
-      1, 2),
-    CHAR 5-9
-    ('Alexander','Ratliff','1967-10-22', '08008719187','enim@icloud.edu',
-     '78','Faucibus St.','Bedfordshire','GL8 4DD',
-     1,2),
-    ('Demetria','Sutton','1986-11-21', '01513565815','dignissim@google.net',
-     '45', 'Williams Road','Rutland','S8 6AE',
-     2,12,
-     'Nullam enim. Sed nulla ante, iaculis nec,'),
-    ('Callum','Francis','2001-12-22', '05548858371','vulputate.dui.nec@icloud.co.uk',
-     '45', 'Av Lane', 'Derbyshire','RF6 1AY',
-     3,4);
-    ('Abel','Garrison','1972-02-22', '01487027927','elementum@icloud.com',
-     '82','Magnis St.','Norfolk','W49 5PU',
-     4,8,
-     'a felis ullamcorper viverra. Maecenas iaculis aliquet'),
-    ('Bertha','Bartlett','1993-10-22', '05687602427','parturient@outlook.edu',
-     '56','Fermentum Ave','Merionethshire','U5V 6YP',
-     3,10,
-     'Duis ac arcu. Nunc mauris. Morbi non sapien');
+
 
 
 
