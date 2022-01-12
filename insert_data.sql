4b9b882db0fc6919e52e09f3f73ff4d1d9643757
UP768646 <51681429+UP768646@users.noreply.github.com>
2022-01-12 18:25:41 +0000

    insert statements treatment

--- insert_data.sql
+++ insert_data.sql
@@ -94,6 +94,18 @@ INSERT INTO complaint (complaint_date, complaint_desc) VALUES
     ('2021-01-29', 'Tics, uncontrollable movements and sounds'),
     ('2021-10-18', 'Covid 19 Symptoms unable to treat at home');
 
+    -- TREATMENT
+-- Treatment table works
+INSERT INTO treatment (treatment_start, treatment_end, treatment_desc, complaint_no) VALUES
+    ('2021-05-24', '2021-05-24', 'paracetemol and cough syrup', 1),
+    ('2021-05-25', '2021-06-01', 'Amoxicillin', 2),
+    ('2021-05-26', '2021-06-10', 'Ventilator', 3),
+    ('2022-01-05', '2022-01-12', 'Antibiotics', 4),
+    ('2022-01-01', '2022-01-03', 'Plaster Cast', 5),
+    ('2021-03-07', '2021-03-07', 'Cauterise', 6),
+    ('2021-04-06', '2021-04-06', 'External referal', 7),
+    ('2021-10-18', '2021-11-02', 'Admittance to covid ward', 8);
+
 -- DOCTOR-TREATMENT
 INSERT INTO doctor_treatment (doctor_id, treatment_id) VALUES
 --  combo key (of doctor id, then treatment id), comments
@@ -105,18 +117,7 @@ INSERT INTO doctor_treatment (doctor_id, treatment_id) VALUES
     ((2,6), 'Referal to external diagnostic services - tourettes'),
     ((0,7), 'Admit to covid ward for further treatment');
 
--- TREATMENT
--- Treatment table works
-INSERT INTO treatment (treatment_start, treatment_end, treatment_desc, complaint_no) VALUES
-    ('2021-05-24', '2021-05-24', 'paracetemol and cough syrup', 1),
-    ('2021-05-25', '2021-06-01', 'Amoxicillin', 2),
-    ('2021-05-26', '2021-06-10', 'Ventilator', 3),
-    -- Char
-    ('2022-01-05', '2022-01-12', 'Antibiotics', 4),
-    ('2022-01-01', '2022-01-03', 'Plaster Cast', 5),
-    ('2021-03-07', '2021-03-07', 'Cauterise', 6),
-    ('2021-04-06', '2021-04-06', 'External referal', 7),
-    ('2021-10-18', '2021-11-02', 'Admittance to covid ward', 8);
+
 
 
 
