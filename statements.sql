137b330799b06d0796a89b33a1e239a38e4ed9d6
UP2016988 <72519948+UP2016988@users.noreply.github.com>
2022-01-12 11:35:49 +0000

    inserted first statement but its not finished lol

--- statements.sql
+++ statements.sql
@@ -15,6 +15,8 @@
 
 SELECT patient_id, patient_fname, patient_lname, patient_dob, gp_name, gp_add1, gp_add2, 
 treatment_desc, treatment_start, treatment_end from patient
-WHERE patient.patient_id = 
+WHERE patient.patient_gp = patient.gp_id,
+-- dont know what to do next lol
+
 
 -- need to do a joint on gp name and patient.
