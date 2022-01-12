1d0863fa1ed6c13a9350cbfec8325e8284911c20
UP768646 <51681429+UP768646@users.noreply.github.com>
2022-01-12 18:24:08 +0000

    Complaint insert statements

--- insert_data.sql
+++ insert_data.sql
@@ -82,6 +82,18 @@ INSERT INTO staff_doctor (doctor_id) VALUES
     (6),
     (8);
 
+-- COMPLAINT
+-- Complaint table working
+INSERT INTO complaint (complaint_date, complaint_desc) VALUES
+    ('2021-05-24', 'A dry cough and a flu'),
+    ('2021-05-25', 'Chest infection'),
+    ('2021-05-26', 'Couldnt breathe, unable to stay awake, non responsive.'),
+    ('2022-01-04', 'Difficult birth - high chance of infection'),
+    ('2021-12-31', 'Broken tibia'),
+    ('2021-03-07', 'Unstoppable nosebleed'),
+    ('2021-01-29', 'Tics, uncontrollable movements and sounds'),
+    ('2021-10-18', 'Covid 19 Symptoms unable to treat at home');
+
 -- DOCTOR-TREATMENT
 INSERT INTO doctor_treatment (doctor_id, treatment_id) VALUES
 --  combo key (of doctor id, then treatment id), comments
@@ -106,18 +118,7 @@ INSERT INTO treatment (treatment_start, treatment_end, treatment_desc, complaint
     ('2021-04-06', '2021-04-06', 'External referal', 7),
     ('2021-10-18', '2021-11-02', 'Admittance to covid ward', 8);
 
--- COMPLAINT
--- Complaint table working
-INSERT INTO complaint (complaint_date, complaint_desc) VALUES
-    ('2021-05-24', 'A dry cough and a flu'),
-    ('2021-05-25', 'Chest infection'),
-    ('2021-05-26', 'Couldnt breathe, unable to stay awake, non responsive.'),
-    -- Char
-    ('2022-01-04', 'Difficult birth - high chance of infection'),
-    ('2021-12-31', 'Broken tibia'),
-    ('2021-03-07', 'Unstoppable nosebleed'),
-    ('2021-01-29', 'Tics, uncontrollable movements and sounds'),
-    ('2021-10-18', 'Covid 19 Symptoms unable to treat at home');
+
 
 
 -- PATIENT-COMPLAINT
