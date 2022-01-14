2fdcc6031ac2038a6db1a3dce2ca49702d376dc6
UP2028963 <87422366+UP2028963@users.noreply.github.com>
2022-01-14 10:05:45 +0000

    check query comments

--- statements.sql
+++ statements.sql
@@ -13,9 +13,11 @@
 -- DTS date (Date Treatment Started)
 -- DTE date (Date Treatment Ended)
 
-SELECT patient.patient_fname ,patient.patient_lname, patient_dob, gp_name, gp_add1, gp_add2, complaint_desc, treatment_desc, treatment_start, treatment_end from patient
+SELECT CONCAT(patient.patient_fname, ' ', patient.patient_lname) AS "Patient Full Name", patient_dob, gp_name, gp_add1, gp_add2, complaint_desc, CONCAT_WS(' ', staff_fname, staff_lname) AS "Hospital Doctor", treatment_desc, treatment_start, treatment_end from patient
 JOIN general_practitioner ON patient.patient_gp = general_practitioner.gp_id
 JOIN patient_complaint on patient.patient_id = patient_complaint.patient_id
+-- JOIN staff ON 
+-- we need to have the hospital doctor treating the patient
 JOIN complaint ON complaint.complaint_id = patient_complaint.complaint_id
 JOIN treatment on patient_complaint.complaint_id = treatment.complaint_no
 JOIN doctor_treatment on treatment.treatment_id = doctor_treatment.treatment_id;
