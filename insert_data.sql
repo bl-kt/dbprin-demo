f808dd109859b1cb84ca27de70063f8407b49155
blkt <56833452+bl-kt@users.noreply.github.com>
2022-01-14 12:02:17 +0000

    Staff values +30

--- insert_data.sql
+++ insert_data.sql
@@ -338,5 +338,39 @@ INSERT INTO patient (patient_fname, patient_lname, patient_dob, patient_phone, p
 
 -- PLEASE NO MORE PATIENTS
 
-INSERT INTO staff (staff_fname, staff_lname, staff_dob, staff_phone, staff_email, staff_add1, staff_add2, staff_county, staff_postcode)
-VALUES
+INSERT INTO staff (staff_fname, staff_lname, staff_dob, staff_phone, staff_email, staff_add1, staff_add2, staff_county, staff_postcode) VALUES
+ ("Hoyt","Steele","13-08-1950","076 7525 2458","sed.eu.nibh@yahoo.net","32"," Rock Lawns","Dumfriesshire","SI73 9WH"),
+  ("Belle","Mclaughlin","16-04-1975","0845 46 41","ut@protonmail.edu","32","Ladywell Hills","Clackmannanshire","AB68 7MR"),
+  ("Kaitlin","Simon","05-07-1967","0845 46 43","mauris.quis@protonmail.org","86","Saddlers Meadows","Inverness-shire","RX3 0VF"),
+  ("Vielka","Haney","03-07-1963","(027) 1149 1724","montes.nascetur@aol.net","49","Ladywell Hills","Glamorgan","DT6 8PO"),
+  ("Cedric","Alford","02-05-1957","07872 518810","eu.erat.semper@outlook.com","65","Sandhurst Grove","Argyllshire","I52 4ES"),
+
+  ("Levi","Morrow","13-06-1962","0938 286 3391","ultricies.dignissim@icloud.net","37"," Peel Avenue","Merionethshire","X0 7HG"),
+  ("Nathaniel","Vazquez","25-02-1984","056 0635 1957","non.nisi@google.org","45","Wheatsheaf Fairway","Lanarkshire","G4R 6TN"),
+  ("Edan","Dennis","21-04-1963","07624 019640","non.justo.proin@icloud.edu","34","Melrose Cliff","Argyllshire","V6 5RK"),
+  ("Tyrone","Fisher","04-11-1978","(01215) 26372","sed@protonmail.org","25","Raven Brae","Denbighshire","J2 6OR"),
+  ("Garrison","Gallagher","20-09-1990","(01567) 12654","tellus.sem.mollis@outlook.couk","13","Raven Brae","Herefordshire","K33 8RJ"),
+
+  ("Chase","Duran","01-04-1971","07553 131725","natoque@hotmail.org","74","Saddlers Meadows","Kinross-shire","RK3I 8GE"),
+  ("Fleur","Jefferson","27-07-1999","(0151) 407 8704","cras.eu.tellus@hotmail.com","32","Spinney Passage","Kincardineshire","J4 6DR"),
+  ("Jason","Mays","24-07-1961","(0161) 895 1135","consectetuer@google.edu","28","Waltham Courtyard","Dorset","NL03 3SV"),
+  ("Kenneth","Nixon","31-01-1959","0353 873 2383","dictum@aol.couk","79"," Ringwood Mews","Dumfriesshire","VV18 9NH"),
+  ("Desirae","Welch","22-02-1986","(020) 8179 6547","ac.ipsum@protonmail.couk","87","Stuart Dell","Midlothian","OT3 7FU"),
+
+  ("Alan","Ware","29-05-1962","0800 981147","non.arcu@outlook.org","24","Brackenlea","Renfrewshire","FB3M 6HY"),
+  ("Melvin","Melton","20-11-1980","056 7474 2773","nullam.suscipit@aol.edu","31","Brookhouse Woods","Inverness-shire","HY78 6JW"),
+  ("Yeo","Potter","20-10-1996","(016977) 5546","eu.metus@icloud.couk","27","Nicholson Rise","Lanarkshire","YC6 8IS"),
+  ("Rashad","Rush","03-09-1962","056 2187 1803","cum.sociis@icloud.couk","47","Howden Lodge","Westmorland","VD1 3YP"),
+  ("Jacob","Velez","01-09-1985","070 1811 6623","diam.pellentesque@hotmail.edu","27","Sandhurst Grove","Kent","BV2 4MB"),
+
+    ("Thomas","Shields","15-06-1984","(01676) 146149","sed.nec.metus@outlook.ca","76","Post Office Village","Gloucestershire","A6 7UP"),
+  ("Nero","Marshall","23-03-1967","0845 46 48","orci@google.edu","52","Seaview Road","Ayrshire","JL8 4YO"),
+  ("Libby","Higgins","27-02-1984","056 1164 7282","arcu@outlook.net","50","Dawson Ridge","Aberdeenshire","DL9J 5OO"),
+  ("Freya","Doyle","05-04-1974","0845 46 44","dictum.sapien@protonmail.ca","97","Dundee Terrace","Wiltshire","R57 3UU"),
+  ("Hadassah","Miranda","29-03-1979","0397 874 6865","sed.congue@icloud.edu","39","Stone Court","Banffshire","O83 2XR"),
+
+    ("Carlos","Manning","23-12-1996","0800 1111","cum.sociis@yahoo.couk","77","Shapwick Road","Lancashire","MG53 9UG"),
+  ("Magee","Logan","05-05-1984","(01767) 422465","pede.blandit.congue@aol.edu","73","Brookhouse Woods","Banffshire","V1 3WJ"),
+  ("Kelsey","Benton","05-08-1969","(016977) 1583","nulla@yahoo.couk","91","Nicholson Rise","Gloucestershire","O34 0NU"),
+  ("Ann","Shepherd","23-08-1969","0800 1111","varius.nam@yahoo.net","51","Stone Court","Pembrokeshire","G7U 3SD"),
+  ("Anika","Barr","01-07-1963","0500 224625","ante.dictum@hotmail.org","74","Howden Lodge","Yorkshire","G8 0VQ");
