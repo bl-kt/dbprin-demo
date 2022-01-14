ee6f88d59de98d76798e45a4919d71155f53202a
UP768646 <51681429+UP768646@users.noreply.github.com>
2022-01-14 12:00:03 +0000

    Query doctors patients

--- statements.sql
+++ statements.sql
@@ -24,8 +24,11 @@ JOIN doctor_treatment on treatment.treatment_id = doctor_treatment.treatment_id;
 
 -- dont know what to do next lol
 
+-- need to do a joint on gp name and patient.
 
 -- which patient has left as a sql query
+
+
 
 -- USE STUFF THAT HE THOUGHT THIS YEAR LIKE ILIKE, GROUPBY, ETC
 
@@ -58,3 +61,14 @@ JOIN treatment ON patient_complaint.complaint_id = treatment.complaint_no
 JOIN doctor_treatment on treatment.treatment_id = doctor_treatment.treatment_id
 JOIN staff_doctor ON doctor_treatment.treatment_id = staff_doctor.doctor_id
 JOIN staff on staff.staff_id =  staff_doctor.doctor_id;
+
+-- Which patients are being treated by a particular doctor - E.g. if a doctor is off sick and patients 
+-- need to be allocated to new doctors. Is this too simple? holly working on this 
+SELECT p.patient_id AS "Patient ID", CONCAT(p.patient_fname, ' ', p.patient_lname) AS "Patient Full Name", p.patient_ward AS "Ward", p.patient_comments AS "Comments", c.complaint_desc AS "Complaint", t.treatment_id AS "Treatment ID", t.treatment_desc AS  "Treatment", dt.doctor_id AS "Doctor Name"
+FROM patient p 
+JOIN patient_complaint pc ON p.patient_id = pc.patient_id 
+JOIN complaint c ON pc.complaint_id = c.complaint_id
+JOIN treatment t ON  pc.complaint_id = t.complaint_no 
+JOIN doctor_treatment dt ON t.treatment_id = dt.treatment_id
+JOIN staff_doctor sd ON dt.doctor_id = sd.doctor_id
+WHERE sd.doctor_id = '2';
