4dfff253ead5ad8670c73af732594e6a06aed395
UP2016988 <72519948+UP2016988@users.noreply.github.com>
2022-01-14 11:44:21 +0000

    second query added lets goooooooo

--- statements.sql
+++ statements.sql
@@ -25,9 +25,6 @@ JOIN doctor_treatment on treatment.treatment_id = doctor_treatment.treatment_id;
 -- dont know what to do next lol
 
 
--- need to do a joint on gp name and patient.
-
-
 -- which patient has left as a sql query
 
 -- USE STUFF THAT HE THOUGHT THIS YEAR LIKE ILIKE, GROUPBY, ETC
@@ -54,3 +51,10 @@ JOIN doctor_treatment on treatment.treatment_id = doctor_treatment.treatment_id;
 
 -- DOCTOR RECORD
 -- idea: all previous patients treated by this doctor, for a certain ailment in a certain time period
+
+SELECT patient_fname, patient_lname, CONCAT(staff_fname, ' ' ,staff_lname), treatment_start, treatment_end, treatment_desc FROM patient
+JOIN patient_complaint ON patient.patient_id = patient_complaint.patient_id
+JOIN treatment ON patient_complaint.complaint_id = treatment.complaint_no
+JOIN doctor_treatment on treatment.treatment_id = doctor_treatment.treatment_id
+JOIN staff_doctor ON doctor_treatment.treatment_id = staff_doctor.doctor_id
+JOIN staff on staff.staff_id =  staff_doctor.doctor_id;
