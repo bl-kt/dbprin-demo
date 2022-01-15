dfbcbd984794c4c382d645478176d414bd89f9bb
blkt <char.ryder@blkt.co.uk>
2022-01-15 15:16:57 +0000

    Query 1 and 2 Script

--- queries/query1.sql
+++ queries/query1.sql
@@ -1,15 +1,4 @@
--- admin card for the ward
-
--- Patient No 6 characters (unique) /
--- Patient Name 30 characters / 
--- Date of Birth date (patient's DOB) /
--- GP Name 30 characters (name of patient's GP) /
--- GP Address Address of patient's GP surgery /
--- Complaint 40 character description of complaint /
--- Treatment 40 character description of treatment /
--- Doctor Hospital doctor giving treatment for complaint 
--- DTS date (Date Treatment Started)
--- DTE date (Date Treatment Ended)
+-- PATIENT CARD
 
 SELECT CONCAT(patient.patient_fname, ' ', patient.patient_lname) AS "Patient Full Name", patient_dob, gp_name, gp_add1, gp_add2, complaint_desc, treatment_desc, treatment_start, treatment_end from patient
 JOIN general_practitioner ON patient.patient_gp = general_practitioner.gp_id
@@ -17,3 +6,8 @@ JOIN patient_complaint on patient.patient_id = patient_complaint.patient_id
 JOIN complaint ON complaint.complaint_id = patient_complaint.complaint_id
 JOIN treatment on patient_complaint.complaint_id = treatment.complaint_no
 JOIN doctor_treatment on treatment.treatment_id = doctor_treatment.treatment_id;
+
+-- This query is based off the physical cards that the case study provided us with,
+-- in order to fomuuate our ERD. The business use of the query would be used to display
+-- a patient's treatment history - previous complaints, treatments and who treated
+-- them - alongside other relevant information.
--- queries/query2.sql
+++ queries/query2.sql
@@ -1,3 +1,5 @@
+-- WARD CARD
+
 SELECT ward_name, ward_speciality, CONCAT(patient.patient_fname, ' ', patient.patient_lname) AS "Patient Full Name", CONCAT(staff_fname, ' ', staff_lname) AS "Nurses", is_head_nurse, gp_name, treatment_start FROM patient
 JOIN general_practitioner ON patient.patient_gp = general_practitioner.gp_id
 JOIN patient_complaint on patient.patient_id = patient_complaint.patient_id
@@ -9,3 +11,7 @@ JOIN staff_nurse ON staff_nurse.nurse_ward = ward.ward_id
 JOIN staff ON staff.staff_id = staff_nurse.nurse_id
 ORDER BY treatment_start
 LIMIT 10;
+
+-- This query is based off the physical cards that the case study provided us with,
+-- in order to formulate our ERD. The business use of this query would be to display a
+-- ward's current patients, admittance dates, staff and specialty.
