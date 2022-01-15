d5461328301929d53f488fc0a9aacacb160b0970
UP951062 <UP951062@users.noreply.github.com>
2022-01-15 15:12:29 +0000

    some data

--- insert-statements/treatment.sql
+++ insert-statements/treatment.sql
@@ -1,9 +1,107 @@
 INSERT INTO treatment (treatment_start, treatment_end, treatment_desc, complaint_no) VALUES
-('2021-05-24', '2021-05-24', 'paracetemol and cough syrup', 1),
-('2021-05-25', '2021-06-01', 'Amoxicillin', 2),
-('2021-05-26', '2021-06-10', 'Ventilator', 3),
-('2022-01-05', '2022-01-12', 'Antibiotics', 4),
-('2022-01-01', '2022-01-03', 'Plaster Cast', 5),
-('2021-03-07', '2021-03-07', 'Cauterise', 6),
-('2021-04-06', '2021-04-06', 'External referal', 7),
-('2021-10-18', '2021-11-02', 'Admittance to covid ward', 8);
+  ('2021-02-6','2021-08-5','Immutonin Novovid',1),
+  ('2021-05-1','2021-12-19','Cabothasol',2),
+  ('2021-02-23','2021-11-15','Bioletra Agenegen',3),
+  ('2021-01-5','2021-08-13','Epinelestid',4),
+  ('2021-01-14','2021-11-16','Roxacadren Biopentin',5),
+  ('2021-07-18','2021-07-8','Alcafgomar Abirabisome',6),
+  ('2021-04-23','2021-08-17','Humutrace Doctotoin',7),
+  ('2021-06-3','2021-12-24','Dyphytropin Zonigen',8),
+  ('2021-05-16','2021-12-23','Amopizole',9),
+  ('2021-06-27','2021-11-23','Araxapine Actemide',10);
+
+INSERT INTO treatment (treatment_start, treatment_end, treatment_desc, complaint_no) VALUES
+  ('2021-02-13','2021-11-10','Dorzotelase',11),
+  ('2021-06-3','2021-12-17','Hemamenda',12),
+  ('2021-01-21','2021-11-2','Oxataine Gembutrol',13),
+  ('2021-06-7','2021-03-23','Evomycin',14),
+  ('2021-01-19','2021-11-16','Oxanparin Acyroban',15),
+  ('2021-06-28','2021-11-1','Amoxicillin',16),
+  ('2021-04-2','2021-09-1','Antibiotics',17),
+  ('2021-03-16','2021-08-31','Humutrace Doctotoin',18),
+  ('2021-01-4','2021-07-14','Abracor',19),
+  ('2021-04-13','2021-01-27','Dorzotelase',20);
+
+INSERT INTO treatment (treatment_start, treatment_end, treatment_desc, complaint_no) VALUES
+  ('2021-06-29','2021-01-23','Evomycin',21),
+  ('2021-05-31','2021-06-10','Abracor',22),
+  ('2021-04-24','2021-05-8','External referal',23),
+  ('2021-01-8','2021-11-6','Cabothasol',24),
+  ('2021-05-13','2021-04-21','External referal',25),
+  ('2021-01-17','2021-06-23','Alemracil Benefitadine',26),
+  ('2021-06-28','2021-06-30','paracetemol and cough syrup',27),
+  ('2021-05-10','2021-12-12','Evomycin',28),
+  ('2021-06-3','2021-06-25','Lexiretin',29),
+  ('2021-07-6','2021-03-31','Amopizole',30);
+
+INSERT INTO treatment (treatment_start, treatment_end, treatment_desc, complaint_no) VALUES
+  ('2021-04-2','2021-06-24','Dyphytropin Zonigen',31),
+  ('2021-07-19','2021-02-17','Immutonin Novovid',32),
+  ('2021-02-3','2021-09-19','Hemamenda',33),
+  ('2021-07-8','2021-12-28','Afretaxime',34),
+  ('2021-06-9','2021-02-12','Afretaxime',35),
+  ('2021-01-18','2021-09-11','Araxapine Actemide',36),
+  ('2021-02-24','2021-02-23','Hemamenda',37),
+  ('2021-04-17','2021-04-4','Tamsubutrol Aldarate',38),
+  ('2021-03-1','2021-01-25','Alcafgomar Abirabisome',39),
+  ('2021-03-19','2021-12-19','Hemamenda',40);
+
+INSERT INTO treatment (treatment_start, treatment_end, treatment_desc, complaint_no) VALUES
+  ('2021-07-14','2021-03-23','Norgel',41),
+  ('2021-01-3','2021-01-26','Cabothasol',42),
+  ('2021-02-11','2021-10-23','Immutonin Novovid',43),
+  ('2021-04-12','2021-08-31','Abracor',44),
+  ('2021-05-31','2021-02-24','Ventilator',45),
+  ('2021-01-26','2021-07-20','Hemamenda',46),
+  ('2021-04-22','2021-11-1','Humutrace Doctotoin',47),
+  ('2021-01-25','2021-03-28','Tamsubutrol Aldarate',48),
+  ('2021-03-10','2021-12-31','Norgel',49),
+  ('2021-02-16','2021-07-18','Ventilator',50);
+
+INSERT INTO treatment (treatment_start, treatment_end, treatment_desc, complaint_no) VALUES
+  ('2021-03-17','2021-11-28','Bactelise',51),
+  ('2021-04-29','2021-12-27','Epinelestid',52),
+  ('2021-04-8','2021-01-30','Oxanparin Acyroban',53),
+  ('2021-06-9','2021-08-25','Subodizem Previce',54),
+  ('2021-06-29','2022-01-7','Epinelestid',55),
+  ('2021-07-5','2021-08-8','Abracor',56),
+  ('2021-06-13','2021-11-16','Cabothasol',57),
+  ('2021-06-26','2021-07-18','Oxanparin Acyroban',58),
+  ('2021-07-20','2021-09-23','External referal',59),
+  ('2021-02-6','2022-01-1','Roxacadren Biopentin',60);
+
+INSERT INTO treatment (treatment_start, treatment_end, treatment_desc, complaint_no) VALUES
+  ('2021-06-26','2021-11-5','paracetemol and cough syrup',61),
+  ('2021-06-7','2022-01-12','Roxacadren Biopentin',62),
+  ('2021-01-25','2021-11-21','External referal',63),
+  ('2021-01-13','2021-10-24','Dyphytropin Zonigen',64),
+  ('2021-07-1','2021-03-6','Roxacadren Biopentin',65),
+  ('2021-02-2','2021-09-19','Ventilator',66),
+  ('2021-04-16','2021-09-18','Amoxicillin',67),
+  ('2021-02-16','2021-03-19','Ibupion Actimycin',68),
+  ('2021-02-3','2021-10-7','Tamsubutrol Aldarate',69),
+  ('2021-06-10','2022-01-6','Araxapine Actemide',70);
+
+INSERT INTO treatment (treatment_start, treatment_end, treatment_desc, complaint_no) VALUES
+  ('2021-06-6','2021-08-18','Norgel',71),
+  ('2021-07-11','2021-03-15','Bioletra Agenegen',72),
+  ('2021-01-26','2021-12-26','Acunem',73),
+  ('2021-03-31','2021-05-12','Araxapine Actemide',74),
+  ('2021-06-7','2021-08-22','Tamsubutrol Aldarate',75),
+  ('2021-04-12','2022-01-6','Acunem',76),
+  ('2021-07-5','2021-05-3','Bioletra Agenegen',77),
+  ('2021-06-11','2021-10-27','Alcafgomar Abirabisome',78),
+  ('2021-05-10','2021-07-31','Norgel',79),
+  ('2021-03-2','2021-11-12','paracetemol and cough syrup',80);
+
+INSERT INTO treatment (treatment_start, treatment_end, treatment_desc, complaint_no) VALUES  
+  ('2021-07-18','2021-08-9','Araxapine Actemide',81),
+  ('2021-02-15','2021-03-30','Hemamenda',82),
+  ('2021-05-8','2021-06-30','Amoxicillin',83),
+  ('2021-04-28','2021-12-11','Alemracil Benefitadine',84),
+  ('2021-05-27','2021-02-9','External referal',85),
+  ('2021-07-16','2021-12-21','Hemamenda',86),
+  ('2021-01-14','2021-12-7','Dorzotelase',87),
+  ('2021-02-11','2021-08-27','Roxabinol',88),
+  ('2021-07-12','2021-02-26','paracetemol and cough syrup',89),
+  ('2021-07-12','2021-02-2','Norgel',90);
