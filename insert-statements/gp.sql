510c3fc1395c3f6ed5a44f742519b6e41ed7a62d
blkt <char.ryder@blkt.co.uk>
2022-01-15 15:13:16 +0000

    Fixed GP

--- insert-statements/gp.sql
+++ insert-statements/gp.sql
@@ -1,9 +1,11 @@
-INSERT INTO treatment (treatment_start, treatment_end, treatment_desc, complaint_no) VALUES
-('2021-05-24', '2021-05-24', 'paracetemol and cough syrup', 1),
-('2021-05-25', '2021-06-01', 'Amoxicillin', 2),
-('2021-05-26', '2021-06-10', 'Ventilator', 3),
-('2022-01-05', '2022-01-12', 'Antibiotics', 4),
-('2022-01-01', '2022-01-03', 'Plaster Cast', 5),
-('2021-03-07', '2021-03-07', 'Cauterise', 6),
-('2021-04-06', '2021-04-06', 'External referal', 7),
-('2021-10-18', '2021-11-02', 'Admittance to covid ward', 8);
+INSERT INTO general_practitioner (gp_name, gp_add1, gp_add2, gp_county, gp_postcode) VALUES
+  ('Somers Town Health Centre',
+    'Somerstown Central, Tyseley Road', 'Southsea', 'Hampshire', 'PO5 4EZ'),
+  ('The Village Surgery',
+    'The Square', 'South Redbank', 'Hampshire', 'GU5 7EA'),
+  ('Dickens Gardens Medical Centre',
+    'High Avenue', 'Middlesborough Road', 'Pixon', 'PI9 5YJ'),
+  ('East Route General Practice',
+    '34', 'Yeet Str, Yarnsborough', 'Smidley', 'SM1 H33'),
+  ('Vernsborne Surgery',
+    '65', 'Narnsmouth', 'Hammingdom', 'HA4 L37');
