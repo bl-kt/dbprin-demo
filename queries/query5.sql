3541276acb120a8dbaea3d6f8b4718298d6660e0
UP2016988 <72519948+UP2016988@users.noreply.github.com>
2022-01-19 10:22:43 +0000

    added stuff

--- queries/query5.sql
+++ queries/query5.sql
@@ -5,6 +5,12 @@ JOIN ward AS w
 ON p.patient_ward = w.ward_id
 JOIN ward_speciality AS ws
 ON w.ward_speciality = ws.speciality_id
-WHERE w.speciality_name = 'General'
+WHERE w.speciality_name = 'General';
 
 -- This Query is there to show which patients are currently in a specific ward. 
+
+SELECT CONCAT_WS(patient_fname, patient_lname), treatment_desc, gp_name from patient
+JOIN general_practitioner ON patient_gp = gp_id
+JOIN patient_complaint ON patient.patient_id = patient_complaint.patient_id
+JOIN treatment ON patient_complaint.complaint_id = treatment.complaint_no;
+
