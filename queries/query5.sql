65fcb9d339ec811a6a81a6a4a67104099b33768d
UP2016988 <72519948+UP2016988@users.noreply.github.com>
2022-01-19 10:25:50 +0000

    commented query that doesnt work

--- queries/query5.sql
+++ queries/query5.sql
@@ -1,11 +1,11 @@
-SELECT CONCAT(p.patient_fname, ' ', p.patient_lname) AS "Patient Full Name"
-       w.ward_name AS "Ward Name", ws.speciality_name AS "Ward Speciality"
-FROM patient AS p
-JOIN ward AS w
-ON p.patient_ward = w.ward_id
-JOIN ward_speciality AS ws
-ON w.ward_speciality = ws.speciality_id
-WHERE w.speciality_name = 'General';
+-- SELECT CONCAT(p.patient_fname, ' ', p.patient_lname) AS "Patient Full Name"
+--        w.ward_name AS "Ward Name", ws.speciality_name AS "Ward Speciality"
+-- FROM patient AS p
+-- JOIN ward AS w
+-- ON p.patient_ward = w.ward_id
+-- JOIN ward_speciality AS ws
+-- ON w.ward_speciality = ws.speciality_id
+-- WHERE w.speciality_name = 'General';
 
 -- This Query is there to show which patients are currently in a specific ward. 
 
