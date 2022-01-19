8a494fabae50e2ef6cd042d2ffd9d32df4483cec
UP2016988 <72519948+UP2016988@users.noreply.github.com>
2022-01-19 10:41:17 +0000

    big boy changes lets goooo

--- queries/query5.sql
+++ queries/query5.sql
@@ -9,8 +9,10 @@
 
 -- This Query is there to show which patients are currently in a specific ward. 
 
-SELECT CONCAT_WS(patient_fname, patient_lname), treatment_desc, gp_name from patient
+SELECT CONCAT_WS(patient_fname, patient_lname) AS "patients full name", treatment_desc, gp_name, CONCAT_WS(gp_add1, gp_add2) AS "GP address", treatment_start, treatment_end from patient
 JOIN general_practitioner ON patient_gp = gp_id
 JOIN patient_complaint ON patient.patient_id = patient_complaint.patient_id
-JOIN treatment ON patient_complaint.complaint_id = treatment.complaint_no;
+JOIN treatment ON patient_complaint.complaint_id = treatment.complaint_no
+WHERE treatment_start BETWEEN '2021-01-01' AND '2021-12-31'
+AND treatment_end BETWEEN '2021-06-01' AND '2021-12-31';
 
