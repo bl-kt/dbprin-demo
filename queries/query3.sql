b7305e10d01ed3c27f38101d591ae1db4be774fa
blkt <char.ryder@blkt.co.uk>
2022-01-19 12:31:34 +0000

    Q3 AVG Working

--- queries/query3.sql
+++ queries/query3.sql
@@ -4,22 +4,29 @@
 -- Business Usage: To determine whether patients are being treated fast enough compared to given treatment metrics, and which treatments are taking longer to treat than others
 
 SELECT
-CONCAT(p.patient_fname, ' ', p.patient_lname) AS "Patient Full Name",
-p.patient_ward AS Ward,
--- c.complaint_date as "Complaint Registered",
-DATE_PART('day',c.complaint_date::timestamp - t.treatment_start::timestamp) AS "Complaint Registered to Treatment Start (days)",
--- t.treatment_start as "Treatment Started",
-DATE_PART('day', t.treatment_end::timestamp - t.treatment_start::timestamp) AS "Treatment Length (days)",
--- t.treatment_end as "Treatment Ended",
-p.patient_admitted AS "Admitted Date",
-p.patient_discharged AS "Discharged Date",
-DATE_PART('day',p.patient_discharged::timestamp - p.patient_admitted::timestamp) AS "Total Hospital Stay (days)"
--- AVG(DATE_PART('day',p.patient_discharged::timestamp - p.patient_admitted::timestamp)) AS "Average Patient Stay Length"
--- NOT WORKING      AVG(DATE_PART('days',p.patient_admitted::timestamp - p.patient_discharged::timestamp) - DATE_PART('day',p.patient_admitted::timestamp - p.patient_discharged::timestamp)) AS "Divergence from Average Stay Length"
-FROM patient p
-JOIN patient_complaint pc ON p.patient_id = pc.patient_id
-JOIN complaint c ON c.complaint_id = pc.complaint_id
-JOIN treatment t ON pc.complaint_id = t.complaint_no
-WHERE p.patient_discharged IS NOT NULL
+  CONCAT(p.patient_fname, ' ', p.patient_lname) AS "Patient Full Name",
+  p.patient_ward AS "Ward",
+  DATE_PART('day',c.complaint_date::timestamp - t.treatment_start::timestamp) AS "Complaint Registered to Treatment Start (days)",
+  DATE_PART('day', t.treatment_end::timestamp - t.treatment_start::timestamp) AS "Treatment Start to End (days)",
+  DATE_PART('day',p.patient_discharged::timestamp - p.patient_admitted::timestamp) AS "Patient Admitted to Patient Discharged (days)",
+  ( SELECT AVG(stayLength)
+    FROM (
+      SELECT
+        DATE_PART('day', p.patient_discharged::timestamp - p.patient_admitted::timestamp) as stayLength
+      FROM
+        patient p
+    ) as calcAvg
+  ) as "Average Patient Admitted to Patient Discharged (days)"
+FROM
+  patient p
+JOIN
+  patient_complaint pc ON p.patient_id = pc.patient_id
+JOIN
+  complaint c ON c.complaint_id = pc.complaint_id
+JOIN
+  treatment t ON pc.complaint_id = t.complaint_no
+WHERE
+  p.patient_discharged IS NOT NULL
+ORDER BY p.patient_ward;
 
 -- sub queries and sum
