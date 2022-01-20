faf5e88754bdefec445b5902fc089149def9d4d1
blkt <char.ryder@blkt.co.uk>
2022-01-20 14:15:50 +0000

    Reformat

--- queries/query3.sql
+++ queries/query3.sql
@@ -10,37 +10,22 @@
 -- To summarise, this query can be used to ensure that the hospital staff are treating patients quickly enough - and if there are any problem points, they can be inferred from this query.
 -- The inclusion of patient name and ward are to help infer problem areas, or to excuse outlier cases.
 
-SELECT
-  CONCAT(p.patient_fname, ' ', p.patient_lname) AS "Patient Full Name",
-  p.patient_ward AS "Ward",
-  DATE_PART('day',c.complaint_date::timestamp - t.treatment_start::timestamp) AS "Complaint Registered to Treatment Start (days)",
-  DATE_PART('day', t.treatment_end::timestamp - t.treatment_start::timestamp) AS "Treatment Start to End (days)",
-  DATE_PART('day',p.patient_discharged::timestamp - p.patient_admitted::timestamp) AS "Patient Admitted to Patient Discharged (days)",
-  ( SELECT ROUND(AVG(stayLength))
-    FROM (
-      SELECT
-        DATE_PART('day', p.patient_discharged::timestamp - p.patient_admitted::timestamp) as stayLength
-      FROM
-        patient p
-    ) as calcAvg
-  ) as "Average Patient Admitted to Patient Discharged (days)",
-  ( SELECT ROUND(AVG(stayLength) - DATE_PART('day', p.patient_discharged::timestamp - p.patient_admitted::timestamp))
-    FROM (
-      SELECT
-        DATE_PART('day', p.patient_discharged::timestamp - p.patient_admitted::timestamp) as stayLength
-      FROM
-        patient p
-    ) as calcAvg
-  ) as "Difference from Average (days)"
-FROM
-  patient p
-JOIN
-  patient_complaint pc ON p.patient_id = pc.patient_id
-JOIN
-  complaint c ON c.complaint_id = pc.complaint_id
-JOIN
-  treatment t ON pc.complaint_id = t.complaint_no
-WHERE
-  p.patient_discharged IS NOT NULL
-ORDER BY
-  p.patient_ward;
+SELECT CONCAT(p.patient_fname, ' ', p.patient_lname) AS "Patient Full Name",
+p.patient_ward AS "Ward",
+DATE_PART('day',c.complaint_date::timestamp - t.treatment_start::timestamp) AS "Complaint Registered to Treatment Start (days)",
+DATE_PART('day', t.treatment_end::timestamp - t.treatment_start::timestamp) AS "Treatment Start to End (days)",
+DATE_PART('day',p.patient_discharged::timestamp - p.patient_admitted::timestamp) AS "Patient Admitted to Patient Discharged (days)",
+( SELECT ROUND(AVG(stayLength)) FROM (
+    SELECT DATE_PART('day', p.patient_discharged::timestamp - p.patient_admitted::timestamp) as stayLength
+    FROM patient p ) as calcAvg
+) as "Average Patient Admitted to Patient Discharged (days)",
+( SELECT ROUND(AVG(stayLength) - DATE_PART('day', p.patient_discharged::timestamp - p.patient_admitted::timestamp)) FROM (
+    SELECT DATE_PART('day', p.patient_discharged::timestamp - p.patient_admitted::timestamp) as stayLength
+    FROM patient p ) as calcAvg
+) as "Difference from Average (days)"
+FROM patient p
+JOIN patient_complaint pc ON p.patient_id = pc.patient_id
+JOIN complaint c ON c.complaint_id = pc.complaint_id
+JOIN treatment t ON pc.complaint_id = t.complaint_no
+WHERE p.patient_discharged IS NOT NULL
+ORDER BY p.patient_ward;
