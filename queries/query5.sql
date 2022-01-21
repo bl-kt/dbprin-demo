806a4056d5b0cf6007785a4196e6ef0708f414b2
blkt <56833452+bl-kt@users.noreply.github.com>
2022-01-21 11:34:54 +0000

    capitals ig

--- queries/query5.sql
+++ queries/query5.sql
@@ -3,14 +3,9 @@
 -- This overtime can help the hospital manage how many patients they expect to recieve over a certain period of time and how many surgeries they
 -- expect. 
 
-<<<<<<< HEAD
 -- gurvirs code
 SELECT DISTINCT(gp_name) AS "Name of GP Surgery",
 COUNT(treatment_desc) AS "No. of Treatments Based On Referrals"
-=======
-SELECT DISTINCT(gp_name) AS "name of GP surgery",
-COUNT(treatment_desc) AS "no of treatments based on referrals"
->>>>>>> e495b7e9956a5227d4c7a8d9f651a8112a60848e
 FROM general_practitioner gp
 JOIN patient p ON p.patient_gp = gp.gp_id
 JOIN patient_complaint pc ON p.patient_id = pc.patient_id
